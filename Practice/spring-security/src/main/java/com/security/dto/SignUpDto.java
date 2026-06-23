e8f5e5cd2d35a73cad25b1b71c2c337bf4ce72a5
Bhushan <bhushanlahore03@gmail.com>
2026-06-23 20:16:59 +0530

    Add authentication and user management features including controllers, services, and DTOs

---
+++ Practice/spring-security/src/main/java/com/security/dto/SignUpDto.java
@@ -0,0 +1,14 @@
+package com.security.dto;
+
+import com.security.entities.enums.Role;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignUpDto {
+    private String email;
+    private String password;
+    private String name;
+    private Set<Role> roles;
+}
---
+++ Practice/spring-security/src/main/java/com/security/entities/enums/Role.java
@@ -0,0 +1,8 @@
+package com.security.entities.enums;
+
+public enum Role {
+
+    USER,
+    CREATOR,
+    ADMIN
+}
